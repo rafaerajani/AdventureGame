7950567a324c05e5b0e80e4750ec17e4f5758211
Rafae <rafaerajani@gmail.com>
2025-08-25 14:31:20 +1000

    fixed typo in welcome message

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")()
 let name = prompt("Enter your name: ")
-console.log("Hello ", name, ", welcom to Adventure Time!")
+console.log("Hello ", name, ", welcome to Adventure Time!")
 
 const shouldPlay = prompt("Do you want to play? (yes/no) ")
 console.log(shouldPlay)
