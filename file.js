13ecef6efecfccae64bab22a9f18221302e6b8d7
Rafae <rafaerajani@gmail.com>
2025-08-25 14:30:14 +1000

    initial game, only adding the welcome msg

--- file.js
+++ file.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+const prompt = require("prompt-sync")()
+let name = prompt("Enter your name: ")
+console.log("Hello ", name, ", welcom to Adventure Time!")
+
+const shouldPlay = prompt("Do you want to play? (yes/no) ")
+console.log(shouldPlay)
