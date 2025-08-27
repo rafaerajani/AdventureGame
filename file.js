0f5825208fe595d8b590354e6388e798e2de2522
Rafae <rafaerajani@gmail.com>
2025-08-27 14:22:18 +1000

    adding start functionality

--- file.js
+++ file.js
@@ -1,6 +1,12 @@
 const prompt = require("prompt-sync")()
-let name = prompt("Enter your name: ")
+const name = prompt("Enter your name: ")
 console.log("Hello ", name, ", welcome to Adventure Time!")
 
-const shouldPlay = prompt("Do you want to play? (yes/no) ")
+const shouldPlay = prompt("Do you want to play? (y/n) ")
 console.log(shouldPlay)
+
+if (shouldPlay.toLowerCase() !== "y") {
+  console.log("OK, maybe next time!")
+  process.exit()
+}
+console.log("Let's play!")
